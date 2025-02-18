046871efe397fad8ac555d1a1a27958e50182290
yndoo <isabelle2k@naver.com>
2025-02-19 00:38:32 +0900

    [ Feat ] 상호작용 트리거 제작

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // LayerMask로 여러 Layer 확인 가능!!!
+    [SerializeField] private LayerMask canInteractLayerMask;
+
     private Vector2 movementDirection;
 
     private Rigidbody2D playerRigidbody;
@@ -18,7 +21,6 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Movement(movementDirection);
-
     }
     private void Movement(Vector2 direction)
     {
@@ -26,6 +28,16 @@ public class PlayerController : MonoBehaviour
 
         playerRigidbody.velocity = direction;
         animationHandler.Move(direction);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 상호작용 오브젝트 : canInteractLayerMask에 포함되는 것인지 확인
+        int isInsteract = canInteractLayerMask.value | (1 << collision.gameObject.layer);
+        if (canInteractLayerMask.value == isInsteract)
+        {
+            Debug.Log("상호작용");
+        }
     }
 
     private void OnMove(InputValue inputValue)
