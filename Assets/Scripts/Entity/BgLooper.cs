0c399b9d30cf901aa02b45d6e67c3018021508d9
yndoo <isabelle2k@naver.com>
2025-02-19 13:09:52 +0900

    [ Fix ] 맵 디자인 수정, 반복 맵 오브젝트 1개 더 추가

--- Assets/Scripts/Entity/BgLooper.cs
+++ Assets/Scripts/Entity/BgLooper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BgLooper : MonoBehaviour
 {
-    private int numBgCount = 3;
+    private int numBgCount = 4;
     private const float widthOfBgObject = 18f;
 
     void Start()
@@ -19,6 +19,7 @@ public class BgLooper : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 배경 looper
         if(collision.CompareTag("background"))
         {
             Vector3 pos = collision.transform.position;
@@ -26,6 +27,12 @@ public class BgLooper : MonoBehaviour
             pos.x += widthOfBgObject * numBgCount;
             collision.transform.position = pos;
             return;
+        }
+
+        // 장애물 looper
+        if (collision.CompareTag("obstacle"))
+        {
+
         }
     }
 }
